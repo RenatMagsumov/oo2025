2028c1810f44205cdee48e92bad7a8573408587e
RenatMagsumov <renatik@tlu.ee>
2025-02-25 13:24:47 +0200

    kodutoo2

--- 02_14.02_veateated/kymnevoistlus/kymnevoistlus/src/main/java/ee/renat/kymnevoistlus/controller/AthleteController.java
+++ 02_14.02_veateated/kymnevoistlus/kymnevoistlus/src/main/java/ee/renat/kymnevoistlus/controller/AthleteController.java
@@ -2,7 +2,9 @@ package ee.renat.kymnevoistlus.controller;
 
 import ee.renat.kymnevoistlus.repository.AthleteRepository;
 import ee.renat.kymnevoistlus.entity.Athlete;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,16 +16,47 @@ public class AthleteController
     @Autowired
     private AthleteRepository repository;
 
-    @PostMapping
-    public Athlete createAthlete(@RequestBody Athlete athlete)
+    private final AthleteRepository athleteRepository;
+
+    public AthleteController(AthleteRepository athleteRepository)
     {
-        return repository.save(athlete);
+        this.athleteRepository=athleteRepository;
     }
 
     @GetMapping
     public List<Athlete> getAllAthletes()
     {
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Athlete> getAthleteById(@PathVariable Long id)
+    {
+        return athleteRepository.findById(id).map(ResponseEntity::ok).orElseThrow(() -> new RuntimeException("ID_NOT_FOUND"));
+    }
+
+    @PostMapping
+    public ResponseEntity<Athlete> createAthlete(@RequestBody Athlete athlete)
+    {
+        if (athlete.getName()==null)
+        {
+            throw new RuntimeException("NAME_CANNOT_BE_NULL");
+        }
+        if (athlete.getAge() < 0) {
+            throw new RuntimeException("AGE_CANNOT_BE_NEGATIVE");
+        }
+        return ResponseEntity.ok(athleteRepository.save(athlete));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteAthlete(@PathVariable Long id)
+    {
+        if (!athleteRepository.existsById(id))
+        {
+            throw new RuntimeException("ID_NOT_FOUND");
+        }
+        athleteRepository.deleteById(id);
+        return ResponseEntity.ok("ATHLETE_DELETED");
     }
 
 
--- 02_14.02_veateated/kymnevoistlus/kymnevoistlus/src/main/java/ee/renat/kymnevoistlus/controller/ResultController.java
+++ 02_14.02_veateated/kymnevoistlus/kymnevoistlus/src/main/java/ee/renat/kymnevoistlus/controller/ResultController.java
@@ -5,6 +5,7 @@ import ee.renat.kymnevoistlus.entity.Result;
 import ee.renat.kymnevoistlus.repository.ResultRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -13,17 +14,44 @@ import java.util.List;
 public class ResultController
 {
     @Autowired
-    private ResultRepository repository;
+    private final ResultRepository resultRepository;
 
-    @PostMapping
-    public Result createResult(@RequestBody Result result)
-    {
-        return repository.save(result);
+    public ResultController(ResultRepository resultRepository) {
+        this.resultRepository = resultRepository;
     }
 
     @GetMapping
     public List<Result> getAllResults()
     {
-        return repository.findAll();
+        return resultRepository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Result> getResultById(@PathVariable Long id)
+    {
+        return resultRepository.findById(id).map(ResponseEntity::ok).orElseThrow(() -> new RuntimeException("ID_NOT_FOUND"));
+    }
+
+    @PostMapping
+    public ResponseEntity<Result> createResult(@RequestBody Result result)
+    {
+        if (result.getScore() < 0)
+        {
+            throw new RuntimeException("SCORE_CANNOT_BE_NEGATIVE");
+        }
+        return ResponseEntity.ok(resultRepository.save(result));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteResult(@PathVariable Long id)
+    {
+        if (!resultRepository.existsById(id))
+        {
+            throw new RuntimeException("RESULT_NOT_FOUND");
+        }
+        resultRepository.deleteById(id);
+        return ResponseEntity.ok("RESULT_DELETED");
+    }
+
+
 }
