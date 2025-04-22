67b8f4a32558d6df963bd10d60900cbbd1eab6f4
RenatMagsumov <renatik@tlu.ee>
2025-04-23 00:24:17 +0300

    kodutoo3

--- 06_14.03_front-end/frontend_kymnevoistlus/src/pages/MainPage.tsx
+++ 06_14.03_front-end/frontend_kymnevoistlus/src/pages/MainPage.tsx
@@ -1,43 +1,66 @@
-import { useEffect, useState } from 'react'
-import { Result } from '../models/Result'
-import { Athlete } from '../models/Athlete'
+import { useEffect, useState } from 'react';
+import { Athlete } from '../models/Athlete';
 
-function MainPage() 
+function MainPage() {
+  const [athletes, setAthletes] = useState<Athlete[]>([]);
+  const [countries, setCountries] = useState<string[]>([]);
+  const [totalAthletes, setTotalAthletes] = useState(0);
+  const [page, setPage] = useState(0);
+  const [activeCountry, setActiveCountry] = useState('');
+  const athletesByPage = 3;
 
-{
+  useEffect(() => {
+    fetch("http://localhost:5074/athletes/countries")
+      .then(res => res.json())
+      .then(json => setCountries(json));
+  }, []);
 
-    const [results, setResults] = useState<Result[]>([])
-    const [athletes, setAthletes] = useState<Athlete[]>([])
- //uef => onload
- useEffect(() => {
-   fetch("http://localhost:5074/results")
-             .then(res=>res.json())
-             .then(json=>setResults(json))
- }, []);
- 
- useEffect(() => {
-   fetch("http://localhost:5074/athletes")
-             .then(res=>res.json())
-             .then(json=>setAthletes(json))
- }, []);  
+  useEffect(() => {
+    showByCountry('', 0);
+  }, []);
+
+  function showByCountry(country: string, currentPage: number) {
+    setActiveCountry(country);
+    setPage(currentPage);
+    fetch("http://localhost:5074/athletes/athlete-country?country=" + country + "&size=" + athletesByPage + "&page=" + currentPage)
+      .then(res => res.json())
+      .then(json => {
+        setAthletes(json.content);
+        setTotalAthletes(json.totalElements);
+      });
+  }
+
+  function updatePage(newPage: number) {
+    showByCountry(activeCountry, newPage);
+  }
+
   return (
     <div>
-        {athletes.map(athlete => 
-      <div key={athlete.name}>
-        {athlete.country} {athlete.age}
-      </div> )}
-      <br />
-      <br />
-      {results.map(result => 
-      <div key={result.id}>
-        <div>{result.id}</div>
-        <div>{result.event}</div>
-        <div>{result.score}</div>
-        <div>{result.athlete?.id}</div>
-      </div> )}
+      <button onClick={() => showByCountry('', 0)}>
+        All countries
+      </button>
+
+      {countries.map(country =>
+        <button key={country} onClick={() => showByCountry(country, 0)}>
+          {country}
+        </button>
+      )}
+
+      <br /><br />
+
+      {athletes.map(athlete =>
+        <div key={athlete.id}>
+          <div>{athlete.name}</div>
+          <div>{athlete.country}</div>
+          <div>{athlete.age}</div>
+          <br />
+        </div>
+      )}
+
+      <button disabled={page === 0} onClick={() => updatePage(page - 1)}>Previous</button>
+      <button disabled={page === Math.ceil(totalAthletes / athletesByPage - 1)} onClick={() => updatePage(page + 1)}>Next</button>
     </div>
-    
-  )
+  );
 }
 
-export default MainPage
+export default MainPage;
